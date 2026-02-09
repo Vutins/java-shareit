46529fa289440398a6533bd041b2a09c6659a60c
Polina <nikulschina-p@yandex.ru>
2026-02-09 18:18:55 +0400

    добавление dtoBooking и mapperBooking

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.booking.dto;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.booking.status.Status;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingDto {
+
+    private Long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Long booker;
+    private Long item;
+    private Status status;
 }
---
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.booking.mapper;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.model.Booking;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface BookingMapper {
+
+    BookingDto toDto(Booking booking);
+
+    Booking toEntity(BookingDto bookingDto);
+
+    List<BookingDto> toDtoList(List<Booking> bookings);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateBookingFromDto(BookingDto bookingDto, @MappingTarget Booking booking);
+}
